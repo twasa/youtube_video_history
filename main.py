50cb89f189634d251751b03e66d52fe3ba0c6681
William Wang <twasawang@gmail.com>
2025-01-23 01:04:00 +0800

    catch all exception in main function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from google_api import Google
 
 def main():
-    google_service = Google()
-    google_service.setup()
-    google_service.video_history_creation()
+    try:
+        google_service = Google()
+        google_service.setup()
+        google_service.video_history_creation()
+    except Exception as e:
+        print(str(e))
 
 if __name__ == '__main__':
     main()
